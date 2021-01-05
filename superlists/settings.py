b56b40ace571f593cf5f17b0819a744bd03a5aa1
= <=>
2021-01-05 10:13:57 -0800

    set STATIC_ROOT in settings and disable admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -32,8 +32,8 @@ TEST_RUNNER = 'redgreenunittest.django.runner.RedGreenDiscoverRunner'
 
 INSTALLED_APPS = [
     'lists',
-    'django.contrib.admin',
     'django.contrib.auth',
+    #'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
