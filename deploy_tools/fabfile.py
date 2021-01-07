7f8b5842d55340b42614092db307a585d644baec
= <=>
2021-01-07 12:45:51 -0800

    deployment automation now provisions nginx and gunicorn

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,17 +4,25 @@ from fabric.api import cd, env, local, run
 
 REPO_URL = 'https://github.com/WorldWideDev/TDD-Python-Django-Course.git'
 env.key_filename = ['/home/xdevx/XFiles/Deployment/t420.pem']
-env.hosts = ['ec2-34-212-50-123.us-west-2.compute.amazonaws.com']
+env.hosts = ['ec2-34-222-246-51.us-west-2.compute.amazonaws.com']
 
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
     run(f'mkdir -p {site_folder}')
+    _download_dependancies()
     with cd(site_folder):
         _get_latest_source()
         _update_virtualenv()
         _create_or_update_dotenv()
         _update_static_files()
         _update_database()
+        _provision_nginx()
+        _provision_gunicorn()
+        _initialize_services()
+
+def _download_dependancies():
+    run('sudo apt update')
+    run('sudo apt install python3 python3-venv nginx')
 
 def _get_latest_source():
     if exists('.git'):
@@ -46,4 +54,20 @@ def _update_database():
     run(f'./virtualenv/bin/python manage.py migrate --noinput')
 
 
+def _provision_nginx():
+    run(f'cat ./deploy_tools/nginx.template.conf \
+            | sed "s/DOMAIN/{env.host}/g" \
+            | sudo tee /etc/nginx/sites-available/{env.host}')
+    run(f'sudo ln -s /etc/nginx/sites-available/{env.host} \
+            /etc/nginx/sites-enabled')
 
+def _provision_gunicorn():
+    run(f'cat ./deploy_tools/gunicorn-systemd.template.service \
+            | sed "s/DOMAIN/{env.host}/g" \
+            | sudo tee /etc/systemd/system/gunicorn-{env.host}.service')
+
+def _initialize_services():
+    run('sudo systemctl daemon-reload')
+    run('sudo systemctl reload nginx')
+    run(f'sudo systemctl enable gunicorn-{env.host}')
+    run(f'sudo systemctl start gunicorn-{env.host}')
